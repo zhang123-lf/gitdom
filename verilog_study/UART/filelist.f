fce346b6d51ec9050e2a6162c0bf1dd823bca9d8
zhang123 <zlf991112@163.com>
2023-03-30 10:46:28 +0800

    rx_tb仿真vcs

--- verilog_study/UART/filelist.f
+++ verilog_study/UART/filelist.f
@@ -1,4 +1,7 @@
-./baud_rate_clk.v
-./baud_rate_tb.v
+./rx.v
+./rx_tb.v
+./baud_rate_en.v
+//./baud_rate_clk.v
+//./baud_rate_tb.v
 //./tx_tb.v
 //./tx.v
--- verilog_study/UART/rx_tb.v
+++ verilog_study/UART/rx_tb.v
@@ -2,11 +2,13 @@
 module rv_tb (
 );
     parameter PERIOD = 10;
-    reg clk,rx,bd8_rate,rst;
+    reg clk,tx,bd8_rate,rst;
     wire [7:0] rx_data;
     wire rx_rdy;
 
     reg [6:0]sample_count;
+    reg [1:0] sample_st;
+    reg  [7:0] tx_data=8'h66;
     wire bd_rate;
     initial begin
         clk = 0;
@@ -24,10 +26,37 @@ module rv_tb (
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             sample_count <= 0;
-            rx <= 0;
+            tx <= 0;
+            sample_st <= 0;
         end else begin
-            if (bd8_rate) begin
-                
+            if (bd_rate) begin
+                case (sample_st)
+                    0:begin
+                        sample_count <= sample_count + 1;
+                        tx <= 1;
+                        if (sample_count == 10) begin
+                            sample_count <= 0;
+                            sample_st <= 1;
+                        end
+                    end 
+                    
+                    1:begin
+                        tx <= 0;
+                        sample_st <=2;
+                    end
+
+                    2:begin
+                    while (tx_data) begin
+                        tx <= tx_data[0];
+                        tx_data <= tx_data >> 1;
+                    end
+                    sample_st <= 3;
+                    end
+                    3:begin
+                        tx <= 1;
+                        sample_st <=1;
+                    end
+                endcase
             end
         end
     end
@@ -39,12 +68,21 @@ module rv_tb (
         .tx_bd_en(bd_rate)
     );
 
-    rx rx_inst (
+    rx #(
+        .PARITY("NONE")
+    )
+    rx_inst (
         .rst(rst),
         .clk(clk),
         .bd8_rate(bd8_rate),
         .rx_data(rx_data),
         .rx_rdy(rx_rdy),
-        .rx(rx)
+        .rx(tx)
     );
+
+initial begin
+    $fsdbDumpfile("tb.fsdb");
+    $fsdbDumpvars(0,rx_tb);
+    #1000 $finish;
+end
 endmodule
